bf9787d60e0fa966832df6a0fae843ae47ffe10d
JonTDean <JonathanTomDean@Gmail.com>
2020-08-11 11:30:19 -0400

    Getting Ready for Netlify

---
+++ build/precache-manifest.8b879dcd90ea1973d03e07f517d301fb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30c0ce0efa3aeaec0965aa8d23cd4ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f26ea1c2f7077f7ed56",
+    "url": "/static/css/3.3d270df7.chunk.css"
+  },
+  {
+    "revision": "28de1b572a0f7bc9e32e",
+    "url": "/static/css/main.17ab5be7.chunk.css"
+  },
+  {
+    "revision": "2f26ea1c2f7077f7ed56",
+    "url": "/static/js/3.4f89886d.chunk.js"
+  },
+  {
+    "revision": "26de0f509484dab2486ae69d5b8bb1d7",
+    "url": "/static/js/3.4f89886d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d3c399e0dd1ab2bd5c",
+    "url": "/static/js/4.c096076b.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/4.c096076b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b929b4d98f4eefdee02f",
+    "url": "/static/js/5.376cfe1f.chunk.js"
+  },
+  {
+    "revision": "55689d8a982844321a26a935cdcf2e65",
+    "url": "/static/js/5.376cfe1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3820717355dc8f450a7",
+    "url": "/static/js/6.d9a6fa48.chunk.js"
+  },
+  {
+    "revision": "28de1b572a0f7bc9e32e",
+    "url": "/static/js/main.350578dd.chunk.js"
+  },
+  {
+    "revision": "78af7f341779ec3c8ab9",
+    "url": "/static/js/polyfills-css-shim.9dc2997a.chunk.js"
+  },
+  {
+    "revision": "9a1308bb323cc1ecc903",
+    "url": "/static/js/runtime-main.f791dfb1.js"
+  },
+  {
+    "revision": "c2f51d1dce80afab9db903d6dfdbed87",
+    "url": "/static/media/Brand.c2f51d1d.svg"
+  },
+  {
+    "revision": "bde300d68b5e077aa9fb609f96863a13",
+    "url": "/static/media/bg-masthead.bde300d6.jpg"
+  }
+]);
